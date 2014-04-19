86e93cbf20d2c6d9c7fdf5e9f9d77176eabbb87c
Forrest <light24bulbs@gmail.com>
2014-04-18 20:00:37 -0500

    post creation feature spec done

---
+++ spec/posts/features/post_creation_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+feature "post management" do
+  scenario "User creates a post" do
+    visit "/"
+    click_link "New Post"
+
+    fill_in "Title", with:"My Post"
+    #not doing anything for the post body right now, since it is a bit tough to wait until the javascript loads
+    click_button "Create Post"
+
+    expect(page).to have_text("My Post")
+  end
+end
