bef7e98e45ef73e85cf35cb1d03b00e01dd82451
Forrest <light24bulbs@gmail.com>
2014-04-18 20:15:15 -0500

    formatting, crumbs

--- spec/posts/features/post_creation_spec.rb
+++ spec/posts/features/post_creation_spec.rb
@@ -4,11 +4,9 @@ feature "post management" do
   scenario "User creates a post" do
     visit "/"
     click_link "New Post"
-
     fill_in "Title", with:"My Post"
     #not doing anything for the post body right now, since it is a bit tough to wait until the javascript loads
     click_button "Create Post"
-
     expect(page).to have_text("My Post")
   end
 end
